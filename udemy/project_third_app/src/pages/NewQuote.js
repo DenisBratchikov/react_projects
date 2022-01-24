febc1b6813fb446342bbe623bbe15d5759e1d980
Денис Братчиков <ds.bratchikov@tensor.ru>
2022-01-24 19:06:35 +0300

    react router project

---
+++ udemy/project_third_app/src/pages/NewQuote.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+
+export default function NewQuote() {
+  const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (data) => {
+    sendRequest(data);
+  };
+
+  return (
+    <QuoteForm
+      isLoading={status === "pending"}
+      onAddQuote={addQuoteHandler}></QuoteForm>
+  );
+}
