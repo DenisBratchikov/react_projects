1ed9f4b9cb86b352c6fa20720185f809bb3eb01e
Денис Братчиков <ds.bratchikov@tensor.ru>
2022-01-24 15:31:03 +0300

    router

---
+++ udemy/lectures_ninth_app/src/pages/ProductDetail.js
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+export default function ProductDetail() {
+  const params = useParams();
+  return (
+    <section>
+      <h1>Product Detail</h1>
+    </section>
+  );
+}
